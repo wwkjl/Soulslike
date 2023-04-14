b85805c6346937e19dfdaeded418647bb43b5ee3
wwkjl <wwkjl@naver.com>
2023-04-14 23:28:15 +0900

    Section 9
    Animation State, IK Control Rig

---
+++ Source/Soulslike/Private/Characters/SoulslikeAnimInstance.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/SoulslikeAnimInstance.h"
+#include "Characters/SoulslikeCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void USoulslikeAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	SoulslikeCharacter = Cast<ASoulslikeCharacter>(TryGetPawnOwner());
+	if (SoulslikeCharacter)
+	{
+		SoulslikeCharacterMovement = SoulslikeCharacter->GetCharacterMovement();
+	}
+}
+
+void USoulslikeAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (SoulslikeCharacterMovement)
+	{
+		GroundSpeed = UKismetMathLibrary::VSizeXY(SoulslikeCharacterMovement->Velocity);
+		IsFalling = SoulslikeCharacterMovement->IsFalling();
+	}
+}
---
+++ Source/Soulslike/Public/Characters/SoulslikeAnimInstance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SoulslikeAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOULSLIKE_API USoulslikeAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	class ASoulslikeCharacter* SoulslikeCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	class UCharacterMovementComponent* SoulslikeCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float GroundSpeed;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool IsFalling;
+};
